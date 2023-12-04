d90586d2de970a8c248761de2e2014ccff7d5faa
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-12-04 19:25:00 +0200

    delete put endpoints

---
+++ src/tasks/dto/update-task.dto.ts
@@ -0,0 +1,6 @@
+
+ export class UpdateTaskDto {
+    title: string;
+    description: string;
+    assignedTo: string;
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -9,14 +10,25 @@ export class TasksController {
     }
 
     @Get(':id')
-    findById(@Param() param) : string{
-        return `task with id: ${param.id}`
+    findById(@Param('id') id: number) : string{
+        return `task with id: ${id}`
     }
 
     @Post()
     create(@Body() createTaskDTO: CreateTaskDto): string {
         return `Title: ${createTaskDTO.title} Description: ${createTaskDTO.description} Assigned to: ${createTaskDTO.assignedTo}`;
     }
+
+    @Delete(':id')
+    delete(@Param('id') id: number): string{
+        return `Delete Task with id: ${id}`
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() updateTaskDTO: UpdateTaskDto): string{
+        return `Title: ${updateTaskDTO.title} Description: ${updateTaskDTO.description} Assigned to: ${updateTaskDTO.assignedTo}`;
+    }
 }
+
 
 
