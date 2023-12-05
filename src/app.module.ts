3cdb160c2ebf176043480cfe5c67f12d1566eeca
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-12-05 03:10:41 +0200

    init auth/users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 import config from './config/keys';
 
 @Module({
-  imports: [TasksModule,MongooseModule.forRoot(config.mongoURI)],
+  imports: [TasksModule,MongooseModule.forRoot(config.mongoURI), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
